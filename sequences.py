01aea03afb84ecb93510323a8d9554f79c9bb344
Joao Megale <joaoluizlopesmegale@gmail.com>
2023-05-21 14:15:51 -0300

    atualizado para novo vaep

--- sequences.py
+++ sequences.py
@@ -81,9 +81,7 @@ def Get_Pitch_Zone(x, y):
             return 24
         
 # ler o df de ações com vaep
-df = pd.read_csv(r"C:\Users\joaom\Documents\IC\vaep_England.csv")
-df = df.dropna(subset=['end_x', 'end_y', 'start_x', 'start_y'])
-df = df.reset_index()
+df = pd.read_csv(r"C:\Users\joaom\Documents\IC\codes\final\vaep.csv")
 
 # calcula as zonas de cada ação / separa as ações em sequencias / soma o VAEP por sequencia
 zones = [Get_Pitch_Zone(df['start_x'][0], df['start_y'][0])]
@@ -97,14 +95,14 @@ vaeps_finais = []
 times = []
 for i in range(1, len(df)):
     zones.append(Get_Pitch_Zone(df['start_x'][i], df['start_y'][i]))
-    if (df['team_id'][i - 1] != df['team_id'][i]) or (df['game_id'][i - 1] != df['game_id'][i]) or (df['period_id'][i - 1] != df['period_id'][i]) or (df['result_name'][i-1] == 'fail') or (df['type_name'][i-1] == 'shot'):
+    if (df['team_id'][i - 1] != df['team_id'][i]) or (df['game_id'][i - 1] != df['game_id'][i]) or (df['result_name'][i-1] == 'fail') or (df['type_name'][i-1] == 'shot'):
         seq += 1
         vaep_seq.append(df['vaep_value'][i])
         if len_seq > 1:
             vaeps_finais_long.append(vaep_seq[i-1])
         len_seqs.append(len_seq+1)
         vaeps_finais.append(vaep_seq[i-1])
-        times.append(df['team_name'][i-1])
+        times.append(df['team_id'][i-1])
         len_seq = 0
     else:
         len_seq += 1
@@ -121,6 +119,8 @@ del len_seq, seq, vaep_seq, seqs, zones
 zone_sequences = np.array([], dtype=object)
 event_sequences = []
 event_sequence = []
+action_sequences = []
+action_sequence = []
 num_seq = 0
 zone = df['zone'][0]
 sequencia = str(zone)
@@ -129,28 +129,35 @@ for i in range(1, len(df)):
     if df['num_seq'][i] == df['num_seq'][i-1]:
         sequencia += ' ' + str(zone)
         event_sequence.append(df['original_event_id'][i])
+        action_sequence.append(df['new_action_id'][i])
     else:
         zone_sequences = np.insert(zone_sequences, num_seq, sequencia)
         num_seq += 1
         sequencia = str(zone)
         event_sequences.append(event_sequence)
         event_sequence = [df['original_event_id'][i]]
+        action_sequences.append(action_sequence)
+        action_sequence = [df['new_action_id'][i]]
 zone_sequences = np.insert(zone_sequences, num_seq, sequencia)
 event_sequences.append(event_sequence)
+action_sequences.append(action_sequence)
 del num_seq, sequencia, zone
 
 # adiciona a sequencia da qual cada ação pertence ao dataframe
 zone_sequences_ext = []
 event_sequences_ext = []
+action_sequences_ext = []
 k = 0
 for i in range(len(zone_sequences)):
     for j in range(len_seqs[k]):
         zone_sequences_ext.append(zone_sequences[i])
         event_sequences_ext.append(event_sequences[i])
+        action_sequences_ext.append(action_sequences[i])
     k += 1
     
 df['zone_sequence'] = zone_sequences_ext
-df['event_sequence'] = event_sequences_ext
+df['event_id_sequence'] = event_sequences_ext
+df['action_id_sequence'] = action_sequences_ext
 del i, j, k, zone_sequences_ext
 
 # adiciona o tamanho de cada sequencia ao dataframe
@@ -160,25 +167,30 @@ for i in range(len(len_seqs)):
         len_seqs_aux.append(len_seqs[i])
 df['len_seq'] = len_seqs_aux
 
+# cria o dataframe de sequencias
+
 df_sequences = pd.DataFrame()
 seqs_iteradas = 0
 team = []
 sequence = []
 vaep = []
 events = []
+actions = []
 
 for i in range(1, len(df)):
     if df['num_seq'].iloc[i] != df['num_seq'].iloc[i-1]:
-        team.append(df['team_name'].iloc[i-1])
+        team.append(df['team_id'].iloc[i-1])
         sequence.append(df['zone_sequence'].iloc[i-1])
-        events.append(df['event_sequence'].iloc[i-1])
+        events.append(df['event_id_sequence'].iloc[i-1])
+        actions.append(df['action_id_sequence'].iloc[i-1])
         vaep.append(df['vaep_acumulado'].iloc[i-1])
         seqs_iteradas += 1
 
-df_sequences['team_name'] = team
+df_sequences['team_id'] = team
 df_sequences['zone_sequence'] = sequence
 df_sequences['event_ids'] = events
+df_sequences['action_ids'] = actions
 df_sequences['vaep'] = vaep
 
-# df.to_csv('df_actions.csv')
-# df_sequences.to_csv('df_sequences.csv')
+#df.to_csv('df_actions.csv')
+#df_sequences.to_csv('df_sequences.csv')
